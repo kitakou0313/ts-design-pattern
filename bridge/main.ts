8cad1116d3642624f4010c518a592c8ac6d09a2a
kitakou0313 <kitakou0313@gmail.com>
2024-10-05 15:46:26 +0900

    add:add sample code of bridge pattern

---
+++ bridge/main.ts
@@ -0,0 +1,40 @@
+// 実際の処理が実装されている層
+interface Implementation {
+    operationImplementation(): string
+}
+
+// 制御層
+// 利用する側はこのinterfaceから利用する
+class Abstraction {
+    constructor(protected implementation: Implementation) {
+    }
+
+    public operation(): string {
+        const result = this.implementation.operationImplementation()
+        return `Abstraction: Base operation with ${result}`
+    }
+}
+
+class ConcreteImplementationA implements Implementation {
+    public operationImplementation(): string {
+        return `Operation in ConcreteImplementationA`
+    }
+}
+
+class ConcreteImplementationB implements Implementation {
+    public operationImplementation(): string {
+        return `Operation in ConcreteImplementationB`
+    }
+}
+
+function clientCode(abstraction:Abstraction) {
+    console.log(abstraction.operation())
+}
+
+let implementation = new ConcreteImplementationA()
+let abstraction = new Abstraction(implementation)
+clientCode(abstraction)
+
+implementation = new ConcreteImplementationB()
+abstraction = new Abstraction(implementation)
+clientCode(abstraction)
